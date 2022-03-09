d8c3b3cad28cc86f5db14b31096b5582840c5453
Piotr Majorczyk <piotr.majorczyk@allegro.pl>
2022-03-09 19:47:30 +0100

    Created new project with spring initializr

---
+++ src/main/java/com/majorczyk/piotr/kubernetesdemo/KubernetesDemoApplication.java
@@ -0,0 +1,13 @@
+package com.majorczyk.piotr.kubernetesdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubernetesDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KubernetesDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/majorczyk/piotr/kubernetesdemo/KubernetesDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.majorczyk.piotr.kubernetesdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KubernetesDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
